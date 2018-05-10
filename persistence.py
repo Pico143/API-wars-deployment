4bbc02ed8bd238d9586511d9e693a3384f375c6d
Pico143 <pico19942@gmail.com>
2018-05-10 14:42:18 +0200

    added database (also in gitignore) and config.py, added connection to database to persistence module

--- persistence.py
+++ persistence.py
@@ -1,5 +1,20 @@
 import requests
-import copy
+import psycopg2
+import psycopg2.extras
+from config import config
+
+
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
+
+    return wrapper
+
 
 def get_planets(page):
     if page == 1:
